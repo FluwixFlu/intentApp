cefad4334e27661088abf8355548ce9355f1a34f
FluwixFlu <kable660@gmail.com>
2026-04-18 00:44:45 +0300

    initial commit

---
+++ app/src/main/java/com/kizima/intentapp_kizima/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.kizima.intentapp_kizima
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
